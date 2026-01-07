6bfbd34082cdeee65905c60405e7f52ce98fc724
KarthikBandari-2003 <bandarik2003@gmail.com>
2026-01-07 17:51:57 +0530

    Initial commit

---
+++ EMSS-Frontend/src/Components/Footor.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2026 Employee Management System |</p>
+      <div>
+      <p>
+         | Full Stack Project by <b>Bandari Karthik</b>  
+        <span> || React • Spring Boot • MySQL</span>
+        </p>
+      </div>
+    </footer>
+  );
+}
